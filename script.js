a97b5ddcff71e706d39c5ceea409187b6faf3e07
Oluwagbemiga <samuelifeoluwa540@gmail.com>
2023-03-30 12:00:41 +0200

    commit project

---
+++ script.js
@@ -0,0 +1,15 @@
+// MY CHATAPP
+
+// THE LOGGING IN OF THE USER INTO THE CHATROOM.
+
+// THE FORM FOR SUBMISSION
+const form = document.querySelector("form");
+
+const userName = document.querySelector("");
+
+const password = document.querySelector("");
+
+const submitButton = document.querySelector("");
+
+// FOR VALIDATION.
+const errorMessages = ["This field is required"];
